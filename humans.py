6a76f8c621c81dad9f6e83e39c40c47650eb07e3
Destiny Boyer <destiny@destinyboyer.com>
2017-05-18 15:48:28 -0700

    Minor changes to get everything to work

--- humans.py
+++ humans.py
@@ -2,60 +2,60 @@ import random
 
 class Human:
 
-	min_food = 0.3
-	min_water = 0.3
-	max_food = 1.0
-	max_water = 1.0
-	max_consumption = 0.01
+    min_food = 0.3
+    min_water = 0.3
+    max_food = 1.0
+    max_water = 1.0
+    max_consumption = 0.01
 
-	def __init__(self, speedLvl):
-		self.speed = speedLvl
-		self.set_levels()
+    def __init__(self, speedLvl):
+        self.speed = speedLvl
+        self.set_levels()
 
-	def set_levels(self):
-		self.food = calculate_level(min_food, max_food)
-		self.water = calculate_level(min_water, max_water)
-		self.calc_food()
-		self.calc_water()
-		self.calc_resource_usage()
-		return self
+    def set_levels(self):
+        self.food = calc_food(min_food, max_food)
+        self.water = calc_water(min_water, max_water)
+        self.calc_food()
+        self.calc_water()
+        self.calc_resource_usage()
+        return self
 
-	def calc_food(self):
-		self.food = random.uniform(min_food, max_food)
-		return self
+    def calc_food(self):
+        self.food = random.uniform(min_food, max_food)
+        return self
 
-	def calc_water(self):
-		self.water = random.uniform(min_water, max_water)
-		return self
+    def calc_water(self):
+        self.water = random.uniform(min_water, max_water)
+        return self
 
-	def calc_resource_usage(self):
-		self.dehydrate_rate = self.speed / 2
-		self.starve_rate = self.speed / 2
-		return self
+    def calc_resource_usage(self):
+        self.dehydrate_rate = self.speed / 2
+        self.starve_rate = self.speed / 2
+        return self
 
-	def starve(self):
-		self.food = self.food - self.starve_rate
-		return self
+    def starve(self):
+        self.food = self.food - self.starve_rate
+        return self
 
-	def dehydrate(self):
-		self.water = self.water - self.dehydrate_rate
-		return self
+    def dehydrate(self):
+        self.water = self.water - self.dehydrate_rate
+        return self
 
-	def expire(self):
-		self.starve()
-		self.dehydrate()
-		return self
+    def expire(self):
+        self.starve()
+        self.dehydrate()
+        return self
 
-	def eat(self, amount):
-		if(amount > max_consumption):
-			self.food = self.food + max_consumption
-		else:
-			self.food = self.food + amount
-		return self
+    def eat(self, amount):
+        if(amount > max_consumption):
+            self.food = self.food + max_consumption
+        else:
+            self.food = self.food + amount
+        return self
 
-	def drink(self, amount):
-		if(amount > max_consumption):
-			self.water = self.water + max_consumption
-		else:
-			self.water = self.water + amount
-		return self
+    def drink(self, amount):
+        if(amount > max_consumption):
+            self.water = self.water + max_consumption
+        else:
+            self.water = self.water + amount
+        return self
--- zombie.py
+++ zombie.py
@@ -2,25 +2,25 @@ import random
 
 class Zombie:
 
-	min_hours_till_death = 24.0
-	max_hours_till_death = 72.0
+    min_hours_till_death = 100.0
+    max_hours_till_death = 200.0
 
-	def __init__(self, speedLvl):
-		self.speed = speedLvl
-		self.set_levels()
+    def __init__(self, speedLvl):
+        self.speed = speedLvl
+        self.set_levels()
 
-	def set_levels(self):
-		self.set_decay_rate()
-		self.set_time_till_death()
+    def set_levels(self):
+        self.set_decay_rate()
+        self.set_time_till_death()
 
-	def set_decay_rate(self):
-		self.decay_rate = random.uniform(0.5, 1.5)
-		return self
+    def set_decay_rate(self):
+        self.decay_rate = random.uniform(0.5, 1.5)
+        return self
 
-	def set_time_till_death(self):
-		self.time_till_death = random.randint(min_hours_till_death, max_hours_till_death)
-		return self
+    def set_time_till_death(self):
+        self.time_till_death = random.randint(min_hours_till_death, max_hours_till_death)
+        return self
 
-	def decay(self):
-		self.time_till_death = self.time_till_death - self.decay_rate
-		return self
+    def decay(self):
+        self.time_till_death = self.time_till_death - self.decay_rate
+        return self
