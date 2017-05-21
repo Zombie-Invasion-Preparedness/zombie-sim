7a699548afb2449db1a909a0af5313dc03f8c478
ScottDFerguson <sfthe5@gmail.com>
2017-05-21 12:00:10 -0700

    Comments and eat() for zombie

--- zombie.py
+++ zombie.py
@@ -2,8 +2,11 @@ import random
 
 class Zombie:
 
+    # starting time limit for zombies
     min_hours_till_death = 100.0
     max_hours_till_death = 200.0
+    
+    hours_from_food = 20
 
     def __init__(self, speedLvl):
         self.speed = speedLvl
@@ -12,15 +15,23 @@ class Zombie:
     def set_levels(self):
         self.set_decay_rate()
         self.set_time_till_death()
+        return self
+        
+    def eat(self):
+        self.time_till_death = self.time_till_death + Zombie.hours_from_food
+        return self
 
+    # set the decay rate
     def set_decay_rate(self):
         self.decay_rate = random.uniform(0.5, 1.5)
         return self
 
+    # set the timie left for a zombie
     def set_time_till_death(self):
         self.time_till_death = random.randint(Zombie.min_hours_till_death, Zombie.max_hours_till_death)
         return self
 
+    # increment a zombies time left by its rate of decay
     def decay(self):
         self.time_till_death = self.time_till_death - self.decay_rate
         return self
